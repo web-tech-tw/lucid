0240d9f9bcedfdc02308d8e0d65674bf2857cf44
SuperSonic <supersonic@livemail.tw>
2022-08-14 14:03:01 +0800

    Update
    Former-commit-id: 5dc7d5f44255a27de288d7b86a5a71677c7afeeb

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,60 @@
 <template>
   <div>
+    <div
+      x-data="scrollProgress"
+      class="
+        fixed
+        inline-flex
+        items-center
+        justify-center
+        overflow-hidden
+        rounded-full
+        top-0
+        right-12
+        md:right-1/2
+        2xl:right-2
+      "
+    >
+      <svg class="w-20 h-20">
+        <circle
+          class="text-gray-300"
+          stroke-width="5"
+          stroke="currentColor"
+          fill="transparent"
+          r="30"
+          cx="40"
+          cy="40"
+        ></circle>
+        <circle
+          class="text-blue-600"
+          stroke-width="5"
+          stroke-linecap="round"
+          stroke="currentColor"
+          fill="transparent"
+          r="30"
+          cx="40"
+          cy="40"
+          stroke-dasharray="188.49555921538757"
+          stroke-dashoffset="188.49555921538757"
+        ></circle>
+      </svg>
+      <svg
+        v-if="isShowInitPowerButton"
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6 absolute text-xl text-emerald-700"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M13 10V3L4 14h7v7l9-11h-7z"
+        />
+      </svg>
+      <span v-else class="absolute text-xl text-blue-700"> 0% </span>
+    </div>
     <div class="bg-black h-96 overflow-hidden" @click="documentHandleClickBox">
       <div ref="screenContainer">
         <div id="screen"></div>
@@ -20,16 +75,16 @@
         {{ emulatorExtendedInfo.isPaused ? "恢復" : "暫停" }}
       </button>
       <button
-        class="w-28 text-base font-medium text-gray-600 hover:text-gray-500"
-        @click="documentHandleClickButtonFullScreen"
-      >
-        進入全螢幕
-      </button>
-      <button
         class="w-16 text-base font-medium text-gray-600 hover:text-gray-500"
         @click="documentHandleClickButtonReset"
       >
         重置
+      </button>
+      <button
+        class="w-28 text-base font-medium text-gray-600 hover:text-gray-500"
+        @click="documentHandleClickButtonFullScreen"
+      >
+        進入全螢幕
       </button>
     </div>
     <div class="mt-5 px-3 w-full flex justify-center">
