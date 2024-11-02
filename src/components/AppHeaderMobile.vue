1ff99630fb10b7350e76585c0c2cc35880a68ea5
不知火 Shiranui <supersonic@livemail.tw>
2024-11-02 22:43:21 +0800

    refactor: update theme color
    Former-commit-id: 3e4ce7f2ba6ed3171cdabdcb467d7a457234e774
    Former-commit-id: 83b5479a8efb9d50bdba4fb8c9ddcfc69e33af96

--- src/components/AppHeaderMobile.vue
+++ src/components/AppHeaderMobile.vue
@@ -13,7 +13,7 @@
           </div>
           <div class="-mr-2">
             <button
-              class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+              class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-emerald-500"
               type="button"
             >
               <span class="sr-only">Close menu</span>
